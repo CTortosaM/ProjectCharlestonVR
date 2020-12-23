ce379cc4da85b92e793a5967607134fabdb60f57
Adrian <36443291+AdrHer13@users.noreply.github.com>
2020-12-23 13:55:18 +0100

    Menu, primer commit

---
+++ Assets/Scripts/menu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayButtonClick()
+    {
+        //Codigo que cambia de escena
+        //SceneManager.LoadScene("Nombre", LoadSceneMode.Single);
+        Debug.Log("Play");
+    }
+
+    public void InstruccionesButtonClick()
+    {
+        //Codigo que cambia de escena
+        //SceneManager.LoadScene("Nombre", LoadSceneMode.Additive);
+        Debug.Log("Instrucciones");
+    }
+    
+    public void VideoButtonClick()
+    {
+        //Codigo que cambia de escena
+        //SceneManager.LoadScene("Nombre", LoadSceneMode.Single);
+        Debug.Log("Video");
+    }
+
+    public void CloseButtonClick()
+    {
+        //Codigo que cierra el juego
+        Debug.Log("Close");
+        Application.Quit();
+    }
+}
