a575e24238397cd56c77bb7b64eb4b8b94f81ec7
CTortosaM <cartormi@epsg.upv.es>
2020-12-25 21:02:28 +0100

    Efectos de sonido del proyector

--- Assets/Proyector/Proyector.cs
+++ Assets/Proyector/Proyector.cs
@@ -1,11 +1,15 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Audio;
 
 public class Proyector : MonoBehaviour
 {
+    [SerializeField] private AudioClip proyectorEnMarchaAudio;
+    [SerializeField] private AudioClip proyectorSeParaAudio;
     private Animator animator;
+    private AudioSource audioSource;
 
     [SerializeField] private bool estaEncendido = true;
 
@@ -13,22 +17,32 @@ public class Proyector : MonoBehaviour
     public UnityEvent seHaApagadoElProyector;
 
     // Start is called before the first frame update
-    void Start()
-    {
+
+    void Awake () {
         animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
 
         seHaApagadoElProyector.AddListener(seApaga);
+    }
+    void Start()
+    {
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     public void Apagar() {
         estaEncendido = false;
         this.animator.Play("Apagarse");
+
+        this.audioSource.Stop();
+        this.audioSource.clip = proyectorSeParaAudio;
+        this.audioSource.loop = false;
+        this.audioSource.Play();
+
 
         StartCoroutine("AnunciarApagado");
     }
