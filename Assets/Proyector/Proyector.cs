a4bdf7ff739edb67f7364026b2a4684e2ed9b6ee
CTortosaM <cartormi@epsg.upv.es>
2020-12-25 18:38:00 +0100

    Proyector con animacion de apagado

---
+++ Assets/Proyector/Proyector.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Proyector : MonoBehaviour
+{
+    private Animator animator;
+
+    [SerializeField] private bool estaEncendido = true;
+
+    public bool EstaEncendido { get => estaEncendido;}
+    public UnityEvent seHaApagadoElProyector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        seHaApagadoElProyector.AddListener(seApaga);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Apagar() {
+        estaEncendido = false;
+        this.animator.Play("Apagarse");
+
+        StartCoroutine("AnunciarApagado");
+    }
+
+    private IEnumerator AnunciarApagado() {
+        yield return new WaitForSeconds(2);
+
+        seHaApagadoElProyector.Invoke();
+    }
+
+    private void seApaga() {
+        Debug.Log("Se ha apagado el proyector");
+    }
+}
