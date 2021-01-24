0c90c8035acd4b1d8267127a3b3d4ae865551a7d
Adrian <36443291+AdrHer13@users.noreply.github.com>
2021-01-24 21:38:00 +0100

    FADE IMPLEMENTADO

--- Assets/Portal/LogicaPortal.cs
+++ Assets/Portal/LogicaPortal.cs
@@ -1,24 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class LogicaPortal : MonoBehaviour
 {
+    public Canvas fundidoABlancoCanvas;
+    public GameObject cuadradoFundido;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //cuadradoFundido = fundidoABlancoCanvas.GetComponentInChildren<Color>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        SceneManager.LoadScene("MenuPrincipal", LoadSceneMode.Single);
+        fundidoABlancoCanvas.gameObject.SetActive(true);
+        StartCoroutine(FundidoABlanco());
     }
+
+
+    public IEnumerator FundidoABlanco()
+    {
+        int fadeSpeed = 1;
+        Color objectColor = cuadradoFundido.GetComponent<Image>().color;
+        float fadeAmount;
+        while (cuadradoFundido.GetComponent<Image>().color.a < 1)
+        {
+            fadeAmount = objectColor.a + (fadeSpeed * Time.deltaTime);
+
+            objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
+            cuadradoFundido.GetComponent<Image>().color = objectColor;
+            yield return null;
+
+        }
+
+        fundidoABlancoCanvas.gameObject.SetActive(false);
+        cuadradoFundido.GetComponent<Image>().color = new Color(objectColor.r, objectColor.g, objectColor.b, 0);
+        if (fundidoABlancoCanvas.gameObject.activeSelf == false)
+        {
+            SceneManager.LoadScene("MenuPrincipal");
+            yield return null;
+        }
+
+    }
+
 }
