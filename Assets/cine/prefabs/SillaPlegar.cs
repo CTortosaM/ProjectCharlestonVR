20cc41f7b286b91dc5306d47e3971297641973d0
Víctor White <36444816+vicblab1@users.noreply.github.com>
2021-01-24 18:04:24 +0100

    sillas con animacion

--- Assets/cine/prefabs/SillaPlegar.cs
+++ Assets/cine/prefabs/SillaPlegar.cs
@@ -6,31 +6,39 @@ public class SillaPlegar : MonoBehaviour
 {
 
 	Animator m_Animator;
-	public GameObject player;
+	private bool estaCerrada=false;
+	
 
     // Start is called before the first frame update
     void Start()
     {
         m_Animator = gameObject.GetComponent<Animator>();
-	m_Animator.speed = 2.0f;
+	
     }
 
     // Update is called once per frame
     void Update()
     {
-        if ((gameObject.transform.position - player.transform.position).magnitude < 5.0f)
-		m_Animator.Play("Take 001", 0, 1.0f);
+        
+		
     }
 
-    /*void OnCollisionEnter(Collision collision)
+private void OnTriggerEnter(Collider other)
     {
-        //Check for a match with the specified name on any GameObject that collides with your GameObject
-        if (collision.gameObject.name == "Player")
-        {
+        Debug.Log(other.name);
+        if (other.gameObject.tag == "Mano"){
 
-		 
+	if(estaCerrada){
 
+		m_Animator.Play("Take 001 0");
+	}else{
+		m_Animator.Play("Take 001");
 	}
-    }*/
+
+		estaCerrada= !estaCerrada;
+	
+	 
+	};
+    }
 
 }
